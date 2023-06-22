366446a31cccbe1b025d7bf415477c0504b928ff
Uriel Enrique <urielenrique99@gmail.com>
2023-06-21 19:35:13 -0600

    Improving testing

--- MxMoneyTests/Cases/Mocks/MockFirebaseManager.swift
+++ MxMoneyTests/Cases/Mocks/MockFirebaseManager.swift
@@ -23,6 +23,7 @@ class MockFirebaseManager: FirebaseManagerProtocol {
     var receivedCollectionName: String?
     var receivedId: String?
     var receivedCustomQuery: CustomQuery?
+    var shouldCompleteWith: Result<String, AppError> = .failure(.testing("Something failed"))
 
     func store<T: Encodable>(_ data: T, in collectionName: String, with id: String? = nil) async throws {
         calledMethods.insert(.store)
@@ -39,12 +40,38 @@ class MockFirebaseManager: FirebaseManagerProtocol {
     ) async throws -> [T] {
         calledMethods.insert(.fetch)
         receivedCustomQuery = query
-        return [T]()
+
+        switch shouldCompleteWith {
+        case .success(let fileName):
+
+            if let decodedData: [T] = getDataFromFile(fileName) {
+                return decodedData
+            }
+
+            throw AppError.testing("Some error")
+        case .failure(let error):
+            throw error
+        }
     }
 
-    func fetch<T>(fromCollection collection: FirestoreCollection, whereQueryBetween query: BetweenQuery) async throws -> [T] where T : Decodable {
+    func fetch<T: Decodable>(
+        fromCollection collection: FirestoreCollection,
+        whereQueryBetween query: BetweenQuery
+    ) async throws -> [T] {
+
         calledMethods.insert(.fetchBetween)
-        return [T]()
+
+        switch shouldCompleteWith {
+        case .success(let fileName):
+
+            if let decodedData: [T] = getDataFromFile(fileName) {
+                return decodedData
+            }
+
+            throw AppError.testing("Some error")
+        case .failure(let error):
+            throw error
+        }
     }
 
     func fetchRecent<T: Decodable>(
@@ -53,7 +80,25 @@ class MockFirebaseManager: FirebaseManagerProtocol {
     ) async throws -> [T] {
         calledMethods.insert(.fetchRecent)
         receivedCustomQuery = query
-        return [T]()
+
+        switch shouldCompleteWith {
+        case .success(let fileName):
+
+            if let decodedData: [T] = getDataFromFile(fileName) {
+                return decodedData
+            }
+
+            throw AppError.testing("Some error")
+        case .failure(let error):
+            throw error
+        }
+    }
+
+    private func getDataFromFile<T: Decodable>(_ fileName: String) -> [T]? {
+        let filePath = Bundle.testing.path(forResource: fileName, ofType: "json") ?? ""
+        let data = try? Data(contentsOf: URL(filePath: filePath))
+        let decodedData = try? JSONDecoder().decode([T].self, from: data ?? Data())
+        return decodedData
     }
 
 }
---
+++ MxMoneyTests/Utils/Bundle+Utils.swift
@@ -0,0 +1,12 @@
+//
+//  Bundle+Utils.swift
+//  MxMoneyTests
+//
+//  Created by Uriel Hernandez Gonzalez on 21/06/23.
+//
+
+import Foundation
+
+extension Bundle {
+    static let testing = Bundle(for: MockFirebaseManager.self)
+}
